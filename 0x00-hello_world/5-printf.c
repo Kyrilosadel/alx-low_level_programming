6e8847091e2e8f58b6befac3156910f5a864c34a
kyrilos adel <kyrlsadel04@gmail.com>
2023-03-16 04:27:30 -0700

    dsgd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-   /**
-    *  main - Entry point of the program
-    *
-    * Return: Always 0 (Success)
-    */
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message with proper grammar
+ *              followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
  /* print the string using printf fuction */
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 
-/* return =0 */
+/* Return 0 to indicate successful completion of the program */
 
 return (0);
 
