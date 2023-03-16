5e8b1f811370f6a53b0cc48a5be21ac6fff40246
kyrilos adel <kyrlsadel04@gmail.com>
2023-03-16 04:14:05 -0700

    gggg

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+   /**
+    *  main - Entry point of the program
+    *
+    * Return: Always 0 (Success)
+    */
+
+int main(void)
+
+{
+ /* print the string using printf fuction */
+printf("with proper grammar, but the outcome is a piece of art,");
+
+
+/* return =0 */
+
+return (0);
+
+}
