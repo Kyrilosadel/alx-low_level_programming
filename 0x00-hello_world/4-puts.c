32c901f52fd05f14d870c0dbdab40d4a1e0f3ef7
kyrilos adel <kyrlsadel04@gmail.com>
2023-03-16 01:27:01 -0700

    hyf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle,");
-    return 0; 
+    /* Print the string using the puts function */
+    puts("Programming is like building a multilingual puzzle");
+
+    /* Return 0 to indicate successful completion of the program */
+    return (0);
 }
 
