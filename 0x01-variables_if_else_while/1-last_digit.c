48dfa5b331ad85dab84f69bdf6ce1b3e18509da4
kyrilos adel <kyrlsadel04@gmail.com>
2023-03-18 11:39:47 -0700

    sdfsda

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ if (digitof > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, digitof);
 }
-else if (Lastdigitof == 0)
+else if (digitof == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, digitof);
 }
