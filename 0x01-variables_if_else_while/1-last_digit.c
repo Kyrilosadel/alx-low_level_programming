d2eed5534966dd19c6776af3436a0eed78058adb
kyrilos adel <kyrlsadel04@gmail.com>
2023-03-18 11:45:07 -0700

    sdfsda

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,29 +3,31 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - entry point
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int digitof = n % 10;
+int lastDigit = n % 10;
 
-if (digitof > 5)
+printf("Last digit of %d is %d ", n, lastDigit);
+
+if (lastDigit > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, digitof);
+printf("and is greater than 5\n");
 }
-else if (digitof == 0)
+else if (lastDigit == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, digitof);
+printf("and is 0\n");
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digitof);
+printf("and is less than 6 and not 0\n");
 }
 
 return (0);
