2d07a7d1144612caf3561df42ba369413a2b5992
kyrilos adel <kyrlsadel04@gmail.com>
2023-03-18 11:57:08 -0700

    asff

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,11 +9,11 @@
 
 int main(void)
 {
-int n;
-int lastDigit = n % 10;
+int n, lastDigit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
 
 printf("Last digit of %d is %d ", n, lastDigit);
 
