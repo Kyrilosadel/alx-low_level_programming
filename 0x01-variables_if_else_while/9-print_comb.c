e5b490b621ffed0a26780530d4e45ef01be09b99
kyrilos adel <kyrlsadel04@gmail.com>
2023-03-19 12:04:52 -0700

    dsg

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,28 +4,23 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-int i, j, k;
+int i;
 
 for (i = 0; i < 10; i++)
 {
-for (j = i + 1; j < 10; j++)
+putchar(i + '0');
+
+if (i < 9)
 {
-for (k = j + 1; k < 10; k++)
-{
-putchar('0' + i);
 putchar(',');
 putchar(' ');
-putchar('0' + j);
-putchar(',');
-putchar(' ');
-putchar('0' + k);
-putchar('\n');
-}
 }
 }
 
-return (0);
+putchar('$');
+
+return 0;
 }
 
