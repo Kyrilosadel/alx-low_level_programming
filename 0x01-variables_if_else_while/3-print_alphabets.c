fb8c9a7aba60bdd559c1d58f9f64ee0eeb7e1f88
kyrilos adel <kyrlsadel04@gmail.com>
2023-03-19 10:41:32 -0700

    as

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+int aa;
+for (aa = 'A'; aa <= 'Z' ; aa++)
+{
+putchar(aa);
+}
+putchar('\n');
+return (0);
+
+}
