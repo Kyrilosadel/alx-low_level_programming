4dde0ca6b871acb5386af9492e9dfd98342c3724
kyrilos adel <kyrlsadel04@gmail.com>
2023-04-10 09:48:30 -0700

    ea

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,9 +15,13 @@ char *c;
 while (--argc)
 {
 for (c = argv[argc]; *c; c++)
+{
 if (*c < '0' || *c > '9')
+{
 return (printf("ERROR\n"), 1);
+}
 sum += atoi(argv[argc]);
+}
 }
 printf("%d\n", sum);
 return (0);
