83362e195080e0f32eed87b10ace3b4657db2144
kyrilos adel <kyrlsadel04@gmail.com>
2023-04-20 10:15:48 -0700

    asdf

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,43 +1,4 @@
 #include "variadic_functions.h"
-
-/**
- * print_strings - prints strings, followed by a new line.
- *
- * @separator: the string to be printed between strings.
- * @n: the number of strings passed to the function.
- * @...: the variable arguments list.
- *
- * If separator is NULL, don’t print it. If one of the string is NULL, print
- * (nil) instead. Print a new line at the end of your function.
- */
-void print_strings(const char *separator, const unsigned int n, ...)
-{
-	va_list args;
-	unsigned int i;
-	char *str;
-
-	va_start(args, n);
-
-	for (i = 0; i < n; i++)
-	{
-		str = va_arg(args, char *);
-
-		if (str != NULL)
-			printf("%s", str);
-		else
-			printf("(nil)");
-
-		if (i != n - 1 && separator != NULL)
-			printf("%s", separator);
-	}
-
-	printf("\n");
-
-	va_end(args);
-}
-Here's an example of the `print_strings` function and a `main` function that calls it, all in one file:
-
-```c
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -46,7 +7,7 @@ Here's an example of the `print_strings` function and a `main` function that cal
  * @separator: the string to be printed between the strings.
  * @n: the number of strings passed to the function.
  *
- * Return: void
+ * Return: void.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -65,18 +26,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", str);
 
-		if (separator != NULL && i < n - 1)
+		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 	}
 
-	va_end(args);
-
 	printf("\n");
+	va_end(args);
 }
 
-int main(void)
-{
-	print_strings(", ", 3, "hello", "world", NULL);
-	print_strings(NULL, 2, "foo", "bar");
-	return (0);
-}
