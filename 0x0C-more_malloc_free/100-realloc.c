eb275d04f1f96b4e6104851dfe53bb4b228cccb1
kyrilosadel <kyrlsadel04@gmail.com>
2023-04-24 00:49:33 -0700

    afsd

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+/**
+ * _realloc - Reallocates a memory block using malloc and free
+ *
+ * @ptr: Pointer to the memory previously allocated with a call to malloc
+ * @old_size: The size, in bytes, of the allocated space for ptr
+ * @new_size: The new size, in bytes of the new memory block
+ *
+ * Return: Pointer to the newly allocated memory block
+ *         NULL if new_size == 0 and ptr is not NULL
+ *         NULL if malloc fails or if new_size > 0 and ptr is NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+void *new_ptr;
+
+if (new_size == old_size)
+return (ptr);
+
+if (ptr == NULL)
+{
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+return (NULL);
+else
+return (new_ptr);
+}
+
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+return (NULL);
+
+if (new_size > old_size)
+for (unsigned int i = 0; i < old_size; i++)
+((char *) new_ptr)[i] = ((char *) ptr)[i];
+else
+for (unsigned int i = 0; i < new_size; i++)
+((char *) new_ptr)[i] = ((char *) ptr)[i];
+
+free(ptr);
+
+return (new_ptr);
+}
+
