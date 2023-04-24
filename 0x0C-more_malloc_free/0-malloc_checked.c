0840b289fb9001542f754ecd640dee298704e077
kyrilosadel <kyrlsadel04@gmail.com>
2023-04-24 00:13:37 -0700

    adf

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
-
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Size in bytes to be allocated
+ *
+ * Return: Pointer to the allocated memory
+ *         Exits with status value of 98 if malloc fails
+ */
 void *malloc_checked(unsigned int b)
 {
 void *p;
