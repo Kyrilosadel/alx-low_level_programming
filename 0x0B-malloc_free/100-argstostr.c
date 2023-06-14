3af89be5145b2d42ced35cc735bd7fcddfcf75e7
kyrilos adel <kyrlsaadel04@gmail.com>
2023-06-14 12:06:35 -0700

    sfg

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,37 +10,38 @@
  */
 char *argstostr(int ac, char **av)
 {
-int i, j, total_length = 0;
-char *str;
+	int i, j, total_length = 0;
+	char *str;
+	int k = 0;
 
-if (ac == 0 || av == NULL)
-return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-total_length++;
-}
-total_length += ac;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			total_length++;
+	}
+	total_length += ac;
 
-str = malloc(sizeof(char) * (total_length + 1));
-if (str == NULL)
-return (NULL);
-int k = 0;
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-{
-str[k] = av[i][j];
-k++;
-}
+	str = malloc(sizeof(char) * (total_length + 1));
+	if (str == NULL)
+		return (NULL);
 
-if (str[k] == '\0')
-{
-str[k++] = '\n';
-}
-}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
 
-return (str);
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
+		}
+	}
+
+	return (str);
 }
 
