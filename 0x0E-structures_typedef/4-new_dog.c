f6fa4b617b2ea9f7fe50ecad268984931eec87d2
kyrilos adel <kyrlsaadel04@gmail.com>
2023-06-19 04:10:31 -0700

    agf

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,41 +13,41 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    char *name_copy, *owner_copy;
-    int name_len, owner_len;
+dog_t *new_dog;
+char *name_copy, *owner_copy;
+int name_len, owner_len;
 
-    /* Allocate memory for the new dog */
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+/* Allocate memory for the new dog */
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
 
-    /* Copy the name */
-    name_len = strlen(name) + 1;
-    name_copy = malloc(name_len);
-    if (name_copy == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
-    strcpy(name_copy, name);
+/* Copy the name */
+name_len = strlen(name) + 1;
+name_copy = malloc(name_len);
+if (name_copy == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+strcpy(name_copy, name);
 
-    /* Copy the owner */
-    owner_len = strlen(owner) + 1;
-    owner_copy = malloc(owner_len);
-    if (owner_copy == NULL)
-    {
-        free(name_copy);
-        free(new_dog);
-        return (NULL);
-    }
-    strcpy(owner_copy, owner);
+/* Copy the owner */
+owner_len = strlen(owner) + 1;
+owner_copy = malloc(owner_len);
+if (owner_copy == NULL)
+{
+free(name_copy);
+free(new_dog);
+return (NULL);
+}
+strcpy(owner_copy, owner);
 
-    /* Set the values in the new dog structure */
-    new_dog->name = name_copy;
-    new_dog->age = age;
-    new_dog->owner = owner_copy;
+/* Set the values in the new dog structure */
+new_dog->name = name_copy;
+new_dog->age = age;
+new_dog->owner = owner_copy;
 
-    return (new_dog);
+return (new_dog);
 }
 
