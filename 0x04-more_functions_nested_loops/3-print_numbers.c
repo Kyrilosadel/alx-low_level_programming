4f0a3788c57f5e1a42b9d8e6c3094dddbd5292c2
kyrilos adel <kyrlsaadel04@gmail.com>
2023-06-27 03:39:13 -0700

    sdf

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_numbers - Prints numbers from 0 to 9
+ * print_numbers - Prints numbers from 0 to 9 followed by a new line
  */
 void print_numbers(void)
 {
     int i;
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i <= 9; i++)
     {
-        printf("%d", i);
+        _putchar(i + '0');
     }
-    printf("\n");
+    _putchar('\n');
 }
+
