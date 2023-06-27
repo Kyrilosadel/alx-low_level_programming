bd435d985951162953bee0a683392dbb9ac5702f
kyrilos adel <kyrlsaadel04@gmail.com>
2023-06-27 03:14:44 -0700

    sdf

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+}
+
