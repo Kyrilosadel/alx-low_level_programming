dcf7a401c9585b15f764cf66b6c781c38af5cdbf
kyrilos adel <kyrlsaadel04@gmail.com>
2023-06-30 16:21:19 -0700

    asdf

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,15 @@
 section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 10, 0
+hello db "Hello, Holberton", 0
 
 section .text
-    extern printf
+extern printf
 
 global main
 main:
-    push rbp
-    mov rbp, rsp
+push hello
+push format
+call printf
 
-    push hello
-    push format
-    call printf
-
-    mov rsp, rbp
-    pop rbp
-    mov eax, 0x60
-    xor edi, edi
-    syscall
+xor eax, eax
+ret
 
